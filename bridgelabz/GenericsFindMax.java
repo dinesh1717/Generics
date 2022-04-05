96d221b95910ac5d2e731d03df76a841786b22e9
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-06 00:12:12 +0530

    more than three parameter

--- bridgelabz/GenericsFindMax.java
+++ bridgelabz/GenericsFindMax.java
@@ -1,23 +1,28 @@
 package bridgelabz;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericsFindMax  <T extends Comparable<T>> {
 
 
-    public <T extends Comparable<T>> T findMaximum(T x, T y, T z) {
-        T max = x;
-
-        if (y.compareTo(max) > 0)
-            max = y;
-
-        if (z.compareTo(max) > 0)
-            max = z;
-
+    public <T extends Comparable<T>> T findMaximum(T... x) {
+        T max = x[0];
+        List<T> parameterArray = new ArrayList<T>();
+        for (T element : x) {
+            parameterArray.add(element);
+            if (element.compareTo(max) > 0) {
+                max = element;
+            }
+        }
+        System.out.println("Maximum of " + parameterArray.toString() + " is " + max);
         return max;
     }
 
+
     public static void main(String[] args) {
         GenericsFindMax genericsFindMax = new GenericsFindMax();
-        genericsFindMax.findMaximum(6,7,5);
-        genericsFindMax.findMaximum(6.5,7.8,5.9);
-        genericsFindMax.findMaximum("apple", "peach", "banana");
+        genericsFindMax.findMaximum(6,7,5,9);
+        genericsFindMax.findMaximum(6.5,7.8,5.9,8.6);
+        genericsFindMax.findMaximum("apple", "peach", "banana", "orange");
     }
 }
