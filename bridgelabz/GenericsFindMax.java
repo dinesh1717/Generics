040cbab49eac83b96558e89d89b67fd0e5684515
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-06 00:04:28 +0530

    find Max float added

--- bridgelabz/GenericsFindMax.java
+++ bridgelabz/GenericsFindMax.java
@@ -2,22 +2,22 @@ package bridgelabz;
 public class GenericsFindMax  <T extends Comparable<T>> {
 
 
-    public <T extends Comparable<T>> T findMaximum(T... x) {
-        public <T extends Comparable<T>>T findMaximum (T x, T y, T z){
-            T max = x;
+    public <T extends Comparable<T>> T findMaximum(T x, T y, T z) {
+        T max = x;
 
-            if (y.compareTo(max) > 0)
-                max = y;
+        if (y.compareTo(max) > 0)
+            max = y;
 
-            if (z.compareTo(max) > 0)
-                max = z;
+        if (z.compareTo(max) > 0)
+            max = z;
 
-            return max;
-        }
-        public static void main (String[]args){
-            GenericsFindMax genericsFindMax = new GenericsFindMax();
-            genericsFindMax.findMaximum(6, 7, 5);
-        }
+        return max;
+    }
+
+
+    public static void main(String[] args) {
+        GenericsFindMax genericsFindMax = new GenericsFindMax();
+        genericsFindMax.findMaximum(6,7,5);
+        genericsFindMax.findMaximum(6.5,7.8,5.9);
     }
 }
-
